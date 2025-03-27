29a69401a6370d5459949627f0a181b311093996
Gede Yoga <permana0912@gmail.com>
2025-03-27 10:56:14 +0800

    rename foreign key migration

--- services/prisma/migrations/20250324084848_mapping_user_dan_company/migration.sql
+++ services/prisma/migrations/20250324084848_mapping_user_dan_company/migration.sql
@@ -5,7 +5,7 @@ ALTER TABLE `User` DROP FOREIGN KEY `User_companyId_fkey`;
 ALTER TABLE `User` ADD CONSTRAINT `user_companyId_fkey` FOREIGN KEY (`companyId`) REFERENCES `Company`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- RenameIndex
-ALTER TABLE `User` RENAME INDEX `User_email_key` TO `user_email_key`;
+ALTER TABLE `user` RENAME INDEX `User_email_key` TO `user_email_key`;
 
 -- RenameIndex
-ALTER TABLE `User` RENAME INDEX `User_phone_key` TO `user_phone_key`;
+ALTER TABLE `user` RENAME INDEX `User_phone_key` TO `user_phone_key`;
