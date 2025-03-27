7552b97e2bb887b35252b8f38c741e3a519086d6
Gede Yoga <permana0912@gmail.com>
2025-03-27 10:46:25 +0800

    fix migration

--- services/prisma/migrations/20250324021910_add_nullable_company/migration.sql
+++ services/prisma/migrations/20250324021910_add_nullable_company/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
-ALTER TABLE `company` MODIFY `address` VARCHAR(191) NULL,
+ALTER TABLE `Company` MODIFY `address` VARCHAR(191) NULL,
     MODIFY `phone` VARCHAR(191) NULL,
     MODIFY `email` VARCHAR(191) NULL;
