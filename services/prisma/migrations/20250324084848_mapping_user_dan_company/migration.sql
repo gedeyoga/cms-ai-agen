f00e049a97f91f1de23346ed49250a816991a622
Gede Yoga <permana0912@gmail.com>
2025-03-27 11:04:47 +0800

    fix foreign key index

--- services/prisma/migrations/20250324084848_mapping_user_dan_company/migration.sql
+++ services/prisma/migrations/20250324084848_mapping_user_dan_company/migration.sql
@@ -4,8 +4,11 @@ ALTER TABLE `User` DROP FOREIGN KEY `User_companyId_fkey`;
 -- AddForeignKey
 ALTER TABLE `User` ADD CONSTRAINT `user_companyId_fkey` FOREIGN KEY (`companyId`) REFERENCES `Company`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- RenameIndex
-ALTER TABLE `user` RENAME INDEX `User_email_key` TO `user_email_key`;
 
--- RenameIndex
-ALTER TABLE `user` RENAME INDEX `User_phone_key` TO `user_phone_key`;
+ALTER TABLE `User` DROP INDEX `User_email_key`;
+
+ALTER TABLE `User` ADD UNIQUE INDEX `user_email_key` (`email`);
+
+ALTER TABLE `User` DROP INDEX `User_phone_key`;
+
+ALTER TABLE `User` ADD UNIQUE INDEX `user_phone_key` (`phone`);
